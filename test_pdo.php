0373cd39cfcae23ca61417e92a56e0b4cadee999
Weslley Marçal <weslleymarcaldev@gmail.com>
2025-07-15 22:24:41 -0300

    remove: arquivos sensíveis ou desnecessários

--- test_pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=idv", "weslley", "kuo8jli79");
-    echo "✅ Conexão PDO funcionando!";
-} catch (PDOException $e) {
-    echo "Erro: " . $e->getMessage();
-}
